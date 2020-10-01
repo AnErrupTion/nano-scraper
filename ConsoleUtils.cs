8b2438e0a1eca8658551c0122410338eb4d2d1a4
ShiningLea <49339966+AnErrupTion@users.noreply.github.com>
2020-10-01 14:45:35 +0200

    Add Log() function

--- ConsoleUtils.cs
+++ ConsoleUtils.cs
@@ -4,9 +4,9 @@ namespace B3RAP_Leecher_v3
 {
     public static class ConsoleUtils
     {
-        public static ConsoleColor InfoColor = ConsoleColor.White;
-        public static ConsoleColor BadColor = ConsoleColor.Red;
-        public static ConsoleColor GoodColor = ConsoleColor.Green;
+        private static ConsoleColor InfoColor = ConsoleColor.White;
+        private static ConsoleColor BadColor = ConsoleColor.Red;
+        private static ConsoleColor GoodColor = ConsoleColor.Green;
 
         public static void UpdateConsoleTitle()
         {
@@ -18,12 +18,31 @@ namespace B3RAP_Leecher_v3
                 $"| Retry : {Program.retry}";
         }
 
-        public static void ColorWriteLine(this string msg, ConsoleColor color)
+        private static void ColorWriteLine(this string msg, ConsoleColor color)
         {
             ConsoleColor reset = Console.ForegroundColor;
             Console.ForegroundColor = color;
             Console.WriteLine(msg);
             Console.ForegroundColor = reset;
+        }
+
+        public static void Log(string message, int type)
+        {
+            if (type == 0)
+            {
+                message = message.Insert(0, "[*] ");
+                message.ColorWriteLine(InfoColor);
+            }
+            else if (type == 1)
+            {
+                message = message.Insert(0, "[+] ");
+                message.ColorWriteLine(GoodColor);
+            }
+            else
+            {
+                message = message.Insert(0, "[-] ");
+                message.ColorWriteLine(BadColor);
+            }
         }
     }
 }
