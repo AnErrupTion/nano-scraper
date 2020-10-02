8a933e3e87ab1efb20c80ad2de0ddd1f68b326e0
ShiningLea <49339966+AnErrupTion@users.noreply.github.com>
2020-10-02 14:42:52 +0200

    Update ConsoleUtils to Utils

---
+++ Utils.cs
@@ -0,0 +1,97 @@
+using Leaf.xNet;
+using System;
+
+namespace B3RAP_Leecher_v3
+{
+    public enum LogType
+    {
+        Info = 0,
+        Success = 1,
+        Error = 2
+    }
+
+    public static class Utils
+    {
+        private static readonly ConsoleColor InfoColor = ConsoleColor.White;
+        private static readonly ConsoleColor BadColor = ConsoleColor.Red;
+        private static readonly ConsoleColor GoodColor = ConsoleColor.Green;
+
+        public static void UpdateConsoleTitle()
+        {
+            Console.Title = $"nαnσ scɾαρҽɾ v{Program.Version} " +
+                $"| Engine : {Program.engine} " +
+                $"| Website : {Program.website} " +
+                $"| Keyword : {Program.keyword} " +
+                $"| Errors : {Program.errors} " +
+                $"| Retry : {Program.retry}";
+        }
+
+        private static void ColorWriteLine(this string msg, ConsoleColor color)
+        {
+            ConsoleColor reset = Console.ForegroundColor;
+            Console.ForegroundColor = color;
+            Console.WriteLine(msg);
+            Console.ForegroundColor = reset;
+        }
+
+        public static string GetString(this HttpResponse response)
+        {
+            using var stream = response.ToMemoryStream();
+            return response.CharacterSet.GetString(stream.GetBuffer(), 0, (int)stream.Length);
+        }
+
+        public static HttpRequest CreateRequest(int timeout, int retries, ProxyClient client)
+        {
+            HttpRequest req = new HttpRequest()
+            {
+                UserAgent = Http.ChromeUserAgent(),
+                EnableEncodingContent = false,
+                IgnoreInvalidCookie = true,
+                IgnoreProtocolErrors = true,
+                UseCookies = false,
+                ConnectTimeout = timeout,
+                ReadWriteTimeout = timeout,
+                AllowAutoRedirect = true,
+                MaximumAutomaticRedirections = 10,
+                Proxy = client,
+                Reconnect = false,
+                KeepAlive = true,
+                KeepAliveTimeout = 7500,
+                MaximumKeepAliveRequests = 40
+            };
+
+            if (retries > 0)
+            {
+                req.Reconnect = true;
+                req.ReconnectDelay = timeout;
+                req.ReconnectLimit = retries;
+            }
+
+            req.SslCertificateValidatorCallback += (sender, certificate, chain, sslPolicyErrors) => true;
+            req.AddHeader("Accept", "*/*");
+
+            return req;
+        }
+
+        public static void Log(string message, LogType type) => Log(message, (int)type);
+
+        public static void Log(string message, int type)
+        {
+            if (type == 0)
+            {
+                message = message.Insert(0, "[*] ");
+                message.ColorWriteLine(InfoColor);
+            }
+            else if (type == 1)
+            {
+                message = message.Insert(0, "[+] ");
+                message.ColorWriteLine(GoodColor);
+            }
+            else
+            {
+                message = message.Insert(0, "[-] ");
+                message.ColorWriteLine(BadColor);
+            }
+        }
+    }
+}
