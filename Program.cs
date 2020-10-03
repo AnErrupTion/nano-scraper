5ec8eee9d4f05915160d09b0f1a4fc6ead80d165
emlin2019 <49819627+emlin2019@users.noreply.github.com>
2020-10-03 14:50:47 +0200

    Changed filename to proper name

--- Program.cs
+++ Program.cs
@@ -21,6 +21,7 @@ namespace B3RAP_Leecher_v3
         // Useful global variables for this class
         public static string engine, website, keyword;
         public static int errors, retry, fileInt;
+        public static string dateTimeFileName;
 
         // File parser used to parse the config
         private static readonly FileParser config = new FileParser()
@@ -154,10 +155,12 @@ namespace B3RAP_Leecher_v3
             Console.Clear();
             Console.Title = "Starting scraper...";
 
+            dateTimeFileName = DateTime.Now.ToString("yyyy.dd.M HH.mm.ss");
             fileInt = rand.Next();
-            path = $"results/{scrapingType}-{fileInt}.txt";
+            //path = $"results/{scrapingType}-{fileInt}.txt";
+            path = $"results/{scrapingType}-{dateTimeFileName}.txt";
 
-            again: try
+        again: try
             {
                 if (customLinks != null && customLinks.Count() > 0) ScrapeResult(customLinks, null);
                 else
